6441aab9c4a96de9ae30cb5f88841b02f658da84
nathy2510 <103401392+nathy2510@users.noreply.github.com>
2024-09-19 08:03:54 -0300

    s

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+criaCartão(
+'Biologia',
+    'O que é zigoto?',
+    'O zigoto é uma célula resultante da união do espermatozoide com o óvulo, marcando o início do desenvolvimento embrionário')
+    
+    criaCartão(
+    'Biologia',
+    'O que são poríferos?',
+    'São animais aquáticos, geralmente marinhos, com corpo cheio de poros. Eles filtram água para obter alimento e oxigênio')
+    
+    criaCartão(
+    'Biologia',
+    'Explique o papel das glândulas salivares na boca e sua importância na digestão',
+    'As glândulas salivares produzem saliva, que contém enzimas como a amilase. A saliva inicia a digestão de carboidratos, protege a mucosa oral e facilita a formação do bolo alimentar')
+    
+    criaCartão(
+    'Biologia',
+    'Explique o processo de filtração glomerular nos rins',
+    'A filtração glomerular ocorre nos glomérulos. Sangue entra nos capilares glomerulares, onde a pressão força água e solutos para dentro da cápsula de Bowman, formando o filtrado que será processado nos túbulos renais')
+
+    
+    criaCartão(
+    'Biologia',
+    'Por que a deficiência de vitamina D pode causar problemas ósseos?',
+    'A vitamina D é crucial para a absorção de cálcio e fósforo no intestino. Sem ela, os ossos podem se tornar frágeis e suscetíveis a doenças como raquitismo e osteoporose')
+    
+    criaCartão(
+    'Português',
+    'O que caracteriza o Quinhentismo no Brasil?',
+   
+'Caracteriza-se pela literatura de informação e catequese, produzida por cronistas portugueses e jesuítas, voltada para descrever o Novo Mundo e converter os indígenas ao cristianismo')
+    
+    criaCartão(
+    'Português',
+    'Como diferenciar sentido conotativo de denotativo?',
+    'A diferença está no uso das palavras: conotativo é figurativo, simbólico, como ele é uma pedra (insensível); denotativo é literal, como a pedra caiu')
+    
+    
+    criaCartão(
+    'Português',
+    'Quando ocorreu o Pré-Modernismo no Brasil?, 
+   
+ 
+    'O Pré-Modernismo no Brasil ocorreu no início do século XX, aproximadamente entre 1902 e 1922, período marcado por grandes transformações sociais, econômicas e políticas, como a urbanização e industrialização')
+    
+    criaCartão(
+    'Geografia',
+    'Como a Doutrina Truman influenciou a política mundial? '
+    'Estabeleceu o comprometimento dos EUA em conter o avanço do comunismo, oferecendo apoio econômico e militar a países ameaçados pela influência soviética')
+    
+    criaCartão(
+    'Geografia',
+    'Qual era o objetivo do Plano Marshall?',
+    'Promover a recuperação econômica da Europa pós-Segunda Guerra, evitando o avanço do comunismo, através de ajuda financeira dos Estados Unidos aos países europeus')
+    
+    criaCartão(
+    História,
+    O que marcou o início do Período Pré-Homérico?’,
+    O início do Período Pré-Homérico foi marcado pela chegada dos povos indo-europeus na Península Balcânica, como os aqueus e jônios’)
+    
+    criaCartão(
+    ‘História’,
+    ‘Qual era a base da sociedade romana?’,
+    ‘A sociedade romana era marcada pela divisão entre patrícios, que eram os nobres e proprietários de terras, e plebeus, que eram os camponeses, artesãos e comerciantes’)
+    
+    criaCartão(
+    ‘História’,
+    ‘Qual era a base da sociedade romana?’,
+    ‘A sociedade romana era marcada pela divisão entre patrícios, que eram os nobres e proprietários de terras, e plebeus, que eram os camponeses, artesãos e comerciantes’)
+    
+    criaCartão(
+    ‘História’,
+    ‘Quais eram os principais objetivos do
+    Fascismo italiano?’,
+    ‘Os principais objetivos eram a criação de um Estado forte e centralizado, a promoção do nacionalismo, o expansionismo territorial e a eliminação de qualquer oposição política’)
+    
+    criaCartão(
+    ‘Química’,
+    ‘Quais elementos pertencem ao grupo dos halogênios?’,
+    ‘Os elementos do grupo dos halogênios são flúor, cloro, bromo, iodo e astato.
+    Esses elementos são altamente reativos e formam sais quando reagem com metais’)
+    
+    criaCartão(
+    ‘Química’,
+    ‘O que se entende por velocidade de reação em cinética química?’,
+    ‘É a medida da rapidez com que os reagentes são consumidos ou os produtos são formados. Depende de fatores como concentração, temperatura e presença de catalisadores’)
+    
+    criaCartão(
+    ‘Química’,
+    ‘Como a concentração dos reagentes afeta a velocidade de uma reação?’,
+    ‘Um aumento na concentração dos reagentes geralmente aumenta a velocidade da reação. Isso ocorre porque há mais moléculas disponíveis para colidir e reagir, aumentando a frequência de colisões efetivas ')
+    
+    criaCartão(
+     'Químicav ',
+     'Como a constante de equilíbrio (K) pode ser afetada por mudanças na temperatura? ',
+     'A constante de equilíbrio (K) varia com a temperatura porque a energia térmica influencia a velocidade das reações diretas e inversas. Reações endotérmicas aumentam K com o aumento da temperatura, enquanto reações exotérmicas diminuem K ')
+    
+    criaCartão(
+     'Química ',
+     'Como a reciclagem de metais pode contribuir para a sustentabilidade ambiental?',
+     'A reciclagem de metais reduz a necessidade de extração de minerais, diminuindo o impacto ambiental da mineração. Além disso, economiza energia e reduz a emissão de gases poluentes, promovendo um ciclo de uso mais sustentável e menos agressivo ao meio ambiente')
+    
+    criaCartão(
+     'Química '
+     'Como identificar isômeros de cadeia? ',
+    'Para identificar isômeros de cadeia, analise a estrutura da cadeia carbônica principal. Verifique se há variações como cadeias lineares,ramificadas, ou se é aberta ou fechada, ou se a cadeia é homogênea ou heterogênea, mantendo a mesma fórmula molecular')
+    
